36f255516046c6d9507524a4686c3786b81d3f48
Garrison Pennington <garrisonpennington99@gmail.com>
2020-09-13 16:32:47 -0700

    Add time_list_from_video_name utility
    The video name is the closest timestamp to when recording actually begins and is the most useful when matching input timestamps to frames

--- utils.py
+++ utils.py
@@ -14,3 +14,8 @@ def update_json(file, data):
 def time_list(secs=None):
     t_inf = time.localtime() if secs is None else time.localtime(secs)
     return [t_inf[3], t_inf[4], t_inf[5]]
+
+
+def time_list_from_video_name(filename):
+    t = filename.split("_")[1].split(";")
+    return [int(e) for e in t]
