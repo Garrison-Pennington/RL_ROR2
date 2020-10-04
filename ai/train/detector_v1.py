18643c472d31608f900023b6d88ef91f2c60551c
Garrison Pennington <garrisonpennington99@gmail.com>
2020-10-03 19:45:31 -0700

    update training file to work with anchors

--- ai/train/detector_v1.py
+++ ai/train/detector_v1.py
@@ -6,15 +6,14 @@ from ai.loaders import YoloFrames
 from ai.network import YOLOv3
 from ai.train import yolo_loss
 
-net = YOLOv3((720, 1280, 3), 17, 2)
+out_grids = ((22, 40), (45, 80), (90, 160))
+training_data = YoloFrames(out_grids, 3, 1)
+# t = training_data[0][1][0]
+net = YOLOv3((720, 1280, 3), 17, 3, num_scales=3)
 model = net.compile_model(
-    loss=yolo_loss(),
+    loss=yolo_loss(training_data.anchors, out_grids),
     optimizer='adam'
 )
-
-out_grids = [(o.shape[1], o.shape[2]) for o in model.outputs]
-training_data = YoloFrames(out_grids, 2, 1)
-t = training_data[0]
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=path.expanduser("~/models/ror2_yolov3_v1"),
                                                  save_weights_only=True,
